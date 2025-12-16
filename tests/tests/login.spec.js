b5b282654a9e723012ac3201cfc4bad5038d5ae0
Karen Castañeda <157784167+karencas98@users.noreply.github.com>
2025-12-16 00:11:57 +0000

    Add login automation tests

--- tests/tests/login.spec.js
+++ tests/tests/login.spec.js
@@ -1,1 +1,22 @@
+const { test, expect } = require('@playwright/test');
+
+test('User can login successfully', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/login');
+
+  await page.fill('#username', 'tomsmith');
+  await page.fill('#password', 'SuperSecretPassword!');
+  await page.click('button[type="submit"]');
+
+  await expect(page.locator('#flash')).toContainText('You logged into a secure area');
+});
+
+test('Login fails with invalid credentials', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/login');
+
+  await page.fill('#username', 'wrongUser');
+  await page.fill('#password', 'wrongPassword');
+  await page.click('button[type="submit"]');
+
+  await expect(page.locator('#flash')).toContainText('Your username is invalid');
+});
 
